400a095db004084bf9a3cedd54d1e4304dbc6232
中野勇貴 <sakeikura325325@gmail.com>
2022-08-21 21:15:17 +0900

    develop-001 コメント修正

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
+///
+/// Theme設定用のServiceクラス
+///
 class ThemeService {
   final _key = 'isDarkMode';
   final _box = GetStorage();
 
+  ///
+  /// ダークモードorライトモードをストレージに保存するメソッド
+  ///
   void saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+
+  ///
+  /// ストレージからダークモードorライトモードを読み込むメソッド
+  ///
   bool _loadThemeFromBox() => _box.read(_key) ?? false;
+
+  ///
+  /// 現在のモード(ダークモードorライトモード)を取得するメソッド
+  ///
   ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+
+  ///
+  /// ダークモードorライトモードを切り替えるメソッド
+  ///
   void switchTheme() {
     Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
     saveThemeToBox(!_loadThemeFromBox());

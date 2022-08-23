2235c7d54e1f7fc7ba9ee8a07cf9994e52924f22
中野勇貴 <sakeikura325325@gmail.com>
2022-08-24 00:11:37 +0900

    develop-002 通知機能の作成 androidが機能しない

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_to_do_app/services/notification_service.dart';
 import 'package:flutter_to_do_app/services/theme_service.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +11,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late NotifyHelper notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    // アクセス権を確認
+    notifyHelper.requestPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +42,14 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: () {
           ThemeService().switchTheme();
+          notifyHelper.displayNotification(
+            title: 'titleTest',
+            body: !Get.isDarkMode
+                ? 'Activated Dark Theme'
+                : 'Activated Light Theme',
+          );
+
+          notifyHelper.scheduledNotification();
         },
         child: const Icon(
           Icons.nightlight_round,
